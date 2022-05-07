170ce72233f11db87bcfb11515197f6acde4e1a1
Damian Krzeminski <krzemdamian@gmail.com>
2022-05-07 16:07:14 +0200

    Fix connection strings

--- src/IdentityServerAspNetIdentity/Program.cs
+++ src/IdentityServerAspNetIdentity/Program.cs
@@ -49,7 +49,7 @@ namespace IdentityServerAspNetIdentity
                 {
                     Log.Information("Seeding database...");
                     var config = host.Services.GetRequiredService<IConfiguration>();
-                    var connectionString = config.GetConnectionString("DefaultConnection");
+                    var connectionString = config.GetConnectionString("PostgresConnection");
                     SeedData.EnsureSeedData(connectionString);
                     Log.Information("Done seeding database.");
                     return 0;
